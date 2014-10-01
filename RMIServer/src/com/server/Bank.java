2e2213a40bf1e39c014d87ca74c20a7fdde01a84
Jack <Jgrier93@gmail.com>
2014-10-01 14:30:34 +0100

    Public static void main for server updated with RMI Security code

--- RMIServer/src/com/server/Bank.java
+++ RMIServer/src/com/server/Bank.java
@@ -1,5 +1,7 @@
 package com.server;
 
+import java.rmi.Naming;
+import java.rmi.RMISecurityManager;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.Date;
@@ -12,6 +14,7 @@ public class Bank extends UnicastRemoteObject implements BankInterface {
 private List<Account> accounts; // users accounts
 public Bank() throws RemoteException
 {
+	super(); 
 }
 public void deposit(int account, int amount) throws RemoteException {
 // implementation code
@@ -27,5 +30,23 @@ return new Statement();
 }
 public static void main(String args[]) throws Exception {
 // initialise Bank server - see sample code in the notes for details
+	try 
+	{  
+		// First reset our Security manager 
+		System.setSecurityManager(new RMISecurityManager()); 
+		System.out.println("Security manager set"); 
+
+		// Create an instance of the local object 
+		Bank BankServer = new Bank(); 
+		System.out.println("Instance of Bankserver created"); 
+	
+		Naming.rebind("Function", BankServer); 
+		System.out.println("Name rebind completed"); 
+		System.out.println("Server ready for requests!"); 
+	} 
+	catch(Exception exc) 
+	{ 
+		System.out.println("Error in main - " + exc.toString()); 
+	} 
 }
 }
