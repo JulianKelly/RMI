82080a7291e61c07f9320c20afed688a4514fce3
Jack <Jgrier93@gmail.com>
2014-10-01 15:37:19 +0100

    MORE Little Stuff

--- RMIServer/src/com/server/Transaction.java
+++ RMIServer/src/com/server/Transaction.java
@@ -7,4 +7,19 @@ public class Transaction
 	private Date DateOfTransaction;
 	private int balance;
 	private String TransType;
+	
+	public void SetTransType(String Trans)
+	{
+		TransType = Trans;
+	}
+	public String getTransType()
+	{
+		return TransType;
+	}
+	
+	public void setDate()
+	{
+		Date currentDate = new Date();
+		DateOfTransaction = currentDate;
+	}
 }
