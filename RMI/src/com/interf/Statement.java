36008f1b7507a67e12bdd8d893e82a85fbdd9dd2
Jack <Jgrier93@gmail.com>
2014-10-01 15:31:00 +0100

    Little Stuff

--- RMI/src/com/interf/Statement.java
+++ RMI/src/com/interf/Statement.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 
+import com.server.Transaction;
+
 public interface Statement extends Serializable {
 public int getAccountnum();  // returns account number associated with this statement
 public Date getStartDate(); // returns start Date of Statement
--- RMIServer/src/com/server/Transaction.java
+++ RMIServer/src/com/server/Transaction.java
@@ -1,7 +1,10 @@
 package com.server;
 
-public class Transaction {
+import java.util.Date;
 
-	
-	
+public class Transaction 
+{
+	private Date DateOfTransaction;
+	private int balance;
+	private String TransType;
 }
